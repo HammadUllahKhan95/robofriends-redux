cd19587d09470252e1033b8b20c62edddfe5089b
hammad-ullah95 <132574898+hammad-ullah95@users.noreply.github.com>
2024-05-25 22:46:32 +0200

    service worker

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import 'tachyons';
 
 
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { requestRobots, searchRobots } from './reducers'
 
 import './index.css';
@@ -33,5 +33,8 @@ root.render(
 //   </Provider>,
 //   document.getElementById('root')
 // );
-registerServiceWorker();
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
